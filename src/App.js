f35f0fe2d58af5b9a81ee2334861f55e331f0edd
Mosigisi <emmanuel.mosigisi@student.moringaschool.com>
2024-08-16 07:44:51 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Quest from './components/Quest';
+import Home from './components/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Bookatopia</h1>
+      <Home/>
+      <Quest />
+    </div>
+  );
+}
+
+export default App;
